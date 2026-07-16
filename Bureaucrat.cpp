3af153dcd50bdaf95ffb6a8c628e47ddd454bd23
Roman Lobun <rrw684@gmail.com>
2026-07-16 10:16:13 +0200

    Bureaucrat.cpp

--- Bureaucrat.cpp
+++ Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: rlobun <rlobun@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/15 07:01:09 by rlobun            #+#    #+#             */
-/*   Updated: 2026/07/15 07:18:31 by rlobun           ###   ########.fr       */
+/*   Updated: 2026/07/16 10:14:36 by rlobun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,22 @@ Buraaucrat::Bureaucrat()
 	grade = 150;
 }
 
-Bureaucrat::Bureaucrat(const std::strig& name, int grade)
+Bureaucrat::Bureaucrat(const std::string& name, int grade)
 {
+
 	if (grade < Bureaucrat::highestGrade)
 		throw(Bureaucrat::GradeTooHighException());
 	else if (grade > Bureaucrat::lowestGrade)
 		throw(Bureaucrat::GradeTooLowException());
 	else
 		this->grade = grade;
+	this->name =name;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+			:name(other.name), grade(other.grade)
+{
+
 }
 
 Bureaucrat::~Bureaucrat()
@@ -53,11 +61,53 @@ int Bureaucrat::getGrade() const
 	return (grade);
 }
 
-void Bureaucrat::incrementGradde()
+void Bureaucrat::incrementGrade()
 {
 	incrementGrade(1);
 }
 
 void Bureaucrat::incrementGrade(int i)
 {
-	if (grade-1 < Bureaucrat::highest
+	if (grade - i < Bureaucrat::highestGrade)
+		throw(Bureaucrat::GradeTooHighException());
+	else
+		grade -= i;
+}
+
+void Bureaucrat::decrementGrade()
+
+{
+	decrementGrade(1);
+}
+
+void Bureaucrat::decrementGrade(int i)
+{
+	if (grade + 1 > Bureaucrat::lowestGrade)
+		throw(Bureaucrat::GradeTooLowException());
+	else
+		grade += i;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() cosnt throw()
+{
+	return ("Bureaucrat: grasde too high");
+}
+
+const char* Bureaucrat::GradeTooLowException::what() cosnt throw()
+{
+	return ("Bureaucrat: grasde too low");
+}
+
+std::ostream& operator<<(std::stream& outputStream, const Bureaucrat& b)
+{
+	outputStream << b.getName() << ", bureaucrat grade " << b.getGrade() << "." << std::endl;
+	return (outputStream);
+}
+
+
+
+
+
+
+
+
